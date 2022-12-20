22b7f91f8b059d01379850dff2d48314acb2af47
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2022-12-20 09:54:37 +0100

    Create and validate test for DataGenerator

---
+++ tests/dataloader/test_dataloader.py
@@ -0,0 +1,14 @@
+from dataloader.datasets import DataGenerator
+
+def test_DataGenerator():
+    try:
+        data_generator = DataGenerator(
+            directory="tiny-imagenet-200/train/",
+            batch_size=128,
+            image_size=(64, 64),
+            shuffle=True,
+            seed=0,
+            validation_split=0.2
+        )
+    except:
+        raise
