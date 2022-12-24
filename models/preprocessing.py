48cc1ae64d98209b03e75918b42aa7ed1136261b
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2022-12-24 17:23:05 +0100

    Remove some image augmentation layers

--- models/preprocessing.py
+++ models/preprocessing.py
@@ -3,15 +3,12 @@ import tensorflow as tf
 
 def get_image_augmentation_layer() -> tf.keras.layers.Layer:    
     list_layers = [
-            tf.keras.layers.RandomHeight(0.3),
-            tf.keras.layers.RandomWidth(0.3),
             tf.keras.layers.RandomZoom(0.3),
             tf.keras.layers.RandomFlip("horizontal"),
             tf.keras.layers.RandomRotation(0.2),
             tf.keras.layers.RandomTranslation(height_factor=0.2, width_factor=0.2),
-            tf.keras.layers.RandomBrightness([-0.8,0.8]),
-            tf.keras.layers.RandomContrast(0.2),
-            tf.keras.layers.RandomFlip("horizontal")
+            tf.keras.layers.RandomBrightness(0.2),
+            tf.keras.layers.RandomContrast(0.2)
         ]
     
     image_augmentation = tf.keras.Sequential(
