47b54820a9329c351d08779aa9c6431c4e28f773
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2022-12-24 11:22:28 +0100

    Implement `resume_training`

--- models/trainer.py
+++ models/trainer.py
@@ -12,18 +12,14 @@ from utils.plot import plot_learning_curve
 logger = logging.getLogger(__name__)
 
 
-def train(config_filepath: str, data_generator: DataGenerator) -> Path:
-    # ---- Load config ---
-    config = load_config(config_filepath)
-    
-    
-    # --- Load data ---
+def train(config: dict, data_generator: DataGenerator) -> Path:    
+    # --- Load datasets ---
     ds_train, ds_val = data_generator.train, data_generator.val
-    
     
     # --- Load the model ---
     model = load_and_compile_model(model_name=config["feature_extractor"], dropout=config["dropout"])
     
+    # --- Callbacks ---
     log_dir = Path(f"logs/{config['experiment_name']}/fit/")
     log_dir.mkdir(parents=True, exist_ok=True)
     log_dir = log_dir / datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -37,17 +33,14 @@ def train(config_filepath: str, data_generator: DataGenerator) -> Path:
         tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=5) # type: ignore
     ]
     
-    
     # --- Train the model ---
     history = model.fit(ds_train,
                         epochs=config["epochs"],
                         validation_data=ds_val,
                         callbacks=callbacks)
 
-    
     plot_learning_curve(history.history, fig_savepath=f"logs/{config['experiment_name']}/learning_curve.png")
     
-
     # --- Save the model ---
     model_dirpath = Path(f"saved_models/{config['experiment_name']}")
     model_dirpath.parent.mkdir(parents=True, exist_ok=True)
@@ -56,3 +49,42 @@ def train(config_filepath: str, data_generator: DataGenerator) -> Path:
     logger.info(f"Successfully saved model at `{model_dirpath}`.")
     
     return model_dirpath
+
+
+def resume_training(model_dirpath: str, config: dict, data_generator: DataGenerator):
+    # ---- Load previous model ---
+    assert Path(model_dirpath).exists()
+    model = tf.keras.models.load_model(model_dirpath)  # type: tf.keras.Model
+    logger.info("Previous trained model was succesfully loaded.")
+    
+    # --- Load datasets ---
+    ds_train, ds_val = data_generator.train, data_generator.val
+    
+    # --- Callbacks ---
+    log_dir = Path(f"logs/{config['experiment_name']}/fit/")
+    log_dir.mkdir(parents=True, exist_ok=True)
+    log_dir = log_dir / datetime.now().strftime("%Y%m%d-%H%M%S")
+    
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(
+            patience=config["early_stopping_patience"],
+            monitor="val_loss",
+            mode="min",
+            restore_best_weights=True),
+        tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=5) # type: ignore
+    ]
+    
+    # --- Train the model ---
+    history = model.fit(ds_train,
+                        epochs=config["epochs"],
+                        validation_data=ds_val,
+                        callbacks=callbacks)
+    
+    plot_learning_curve(history.history, fig_savepath=f"logs/{config['experiment_name']}/learning_curve.png")
+    
+
+    # --- Save the model ---
+    model.save(model_dirpath)
+    logger.info(f"Successfully saved model at `{model_dirpath}`.")
+    
+    return
--- train.py
+++ train.py
@@ -1,8 +1,13 @@
 import typer
+
+from typing import Optional
+
 import tensorflow as tf
+
 from dataloader.datasets import DataGenerator
 from models.projector import save_embeddings_for_tf_projector
-from models.trainer import train
+from models.trainer import resume_training, train
+from utils.helper import load_config
 
 
 DATA_DIRPATH = "tiny-imagenet-200/train/"
@@ -11,22 +16,35 @@ IMAGE_SIZE = (64, 64)
 VALIDATION_SPLIT = 0.2
 
  
-def main(config_filepath: str):
+def main(config_filepath: str=typer.Option(...),
+         resume: Optional[str]=typer.Option(None)):
     if tf.config.list_physical_devices('GPU'):
         print(f"GPU(s) detected: {tf.config.list_physical_devices('GPU')}")
+        
+    print("----------------------------------------------------------------------------------\n\n")
     
+    # ---- Load config ---
+    config = load_config(config_filepath)
+    
+    # --- Load data ---
     data_generator = DataGenerator(
         directory=DATA_DIRPATH,
         batch_size=BATCH_SIZE,
         image_size=IMAGE_SIZE,
         shuffle=True,
-        seed=0,
+        seed=config["seed"],
         validation_split=VALIDATION_SPLIT
     )
-    model_dirpath = train(config_filepath, data_generator=data_generator)
     
+    # --- Training ---
+    if resume:
+        resume_training(model_dirpath=resume, config=config, data_generator=data_generator)
+        model_dirpath = resume  # rename arg for backward compatibility
+    else:
+        model_dirpath = train(config=config, data_generator=data_generator)
+    
+    # --- Visualization ---
     model = tf.keras.models.load_model(model_dirpath)
-    
     save_embeddings_for_tf_projector(model, ds_val=data_generator.val) # type: ignore
     
     return
